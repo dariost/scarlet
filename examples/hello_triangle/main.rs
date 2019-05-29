f182e828ef34807e3f1df611c71fa00dabaeac86
Dario Ostuni <dario.ostuni@gmail.com>
2019-05-30 00:12:34 +0200

    emscripten MVP

--- examples/hello_triangle/main.rs
+++ examples/hello_triangle/main.rs
@@ -1,5 +1,7 @@
 extern crate glad_gles2;
 extern crate glutin;
+#[cfg(target_os = "emscripten")]
+mod emscripten;
 
 use glad_gles2::gl;
 
@@ -9,16 +11,20 @@ fn main() {
         .with_title("Hello triangle!")
         .with_dimensions(glutin::dpi::LogicalSize::new(1280.0, 720.0))
         .with_resizable(false);
+    #[cfg(not(target_os = "emscripten"))]
+    let gl_version = glutin::GlRequest::GlThenGles {
+        opengl_version: (4, 3),
+        opengles_version: (3, 0),
+    };
+    #[cfg(target_os = "emscripten")]
+    let gl_version = glutin::GlRequest::Specific(glutin::Api::WebGl, (2, 0));
     let windowed_context = glutin::ContextBuilder::new()
-        .with_gl(glutin::GlRequest::GlThenGles {
-            opengl_version: (4, 3),
-            opengles_version: (3, 0),
-        })
+        .with_gl(gl_version)
+        .with_vsync(true)
         .build_windowed(window_builder, &events_loop)
         .unwrap();
     let windowed_context = unsafe { windowed_context.make_current().unwrap() };
     gl::load(|s| windowed_context.get_proc_address(s) as *const std::ffi::c_void);
-    let mut should_stop = false;
     unsafe {
         gl::ClearColor(1.0, 0.0, 0.0, 1.0);
         let version = gl::GetString(gl::GL_VERSION);
@@ -29,18 +35,29 @@ fn main() {
                 .unwrap()
         );
     }
-    while !should_stop {
+    let mut cycle = move || {
+        let mut should_stop = false;
         unsafe {
             gl::Clear(gl::GL_COLOR_BUFFER_BIT);
         }
-        events_loop.poll_events(|e| match e {
-            glutin::Event::WindowEvent {
-                event: glutin::WindowEvent::CloseRequested,
-                ..
-            } => should_stop = true,
-            _ => {}
+        events_loop.poll_events(|e| {
+            println!("{:?}", e);
+            match e {
+                glutin::Event::WindowEvent {
+                    event: glutin::WindowEvent::CloseRequested,
+                    ..
+                } => should_stop = true,
+                _ => {}
+            }
         });
         windowed_context.swap_buffers().unwrap();
         std::thread::yield_now();
-    }
+        !should_stop
+    };
+    #[cfg(target_os = "emscripten")]
+    emscripten::set_main_loop_callback(|| {
+        cycle();
+    });
+    #[cfg(not(target_os = "emscripten"))]
+    while cycle() {}
 }
